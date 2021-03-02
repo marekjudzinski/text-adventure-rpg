3ef363c6d201a94d1e2670a846e8d2cb7b5d6fb3
Marek <marekju80@gmail.com>
2021-03-02 14:26:18 +0100

    drobne poprawki w stylowaniu, pierwszy etap podrozy, funckje pozwalajace wyswietlac aktualny etap podrozy

--- game.js
+++ game.js
@@ -1,22 +1,35 @@
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
-//let state = {}
 
 //silnik gry.
 function startGame() {
-    //state = {}
     showTextNode(1)
 }
 
 //funkcja, dzieki ktorej bedziemy widziec aktualny czesc naszej przygody wyswietlanej w formie sciany tekstu
+//argumentem jest index, czyli etam naszej podrozy
 function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode =>
+        textNode.id === textNodeIndex)
+        textElement.innerText = textNode.text
+        while (optionButtonsElement.firstChild){
+            optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
 
+    textNode.options.forEach(option => {
+        if (showOption(option)){
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+    })
 }
-
 //funckja, dzieki ktorej bedziemy widziec jakie opcje (w buttonach) mamy do wyboru
 function showOption(option) {
-
+    return true
 }
 
 function selectOption(option) {
@@ -26,7 +39,18 @@ function selectOption(option) {
 //ELEMENTY NASZEJ PRZYGODY
 const textNodes = [
     {
-
+        id: 1,
+        text: 'Wlasnie sie obudziles. Po wczorajszej wizycie w burdelu oprocz bolu glowy, kurewsko boli cie dupa... Cos ewidentnie poszlo nie tak...',
+        options:[
+            {
+                text: 'Wroc do domu uciech cielesnych',
+                nextText: 2
+            },
+            {
+                text: 'DAB!',
+                nextText: 3
+            }
+        ]
     }
 ]
 
