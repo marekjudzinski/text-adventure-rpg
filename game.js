f953a8e9e40ea9f8ec32e9372009be520fa6fe3b
Marek <marekju80@gmail.com>
2021-03-02 13:49:12 +0100

    szkielet gry

---
+++ game.js
@@ -0,0 +1,33 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+//let state = {}
+
+//silnik gry.
+function startGame() {
+    //state = {}
+    showTextNode(1)
+}
+
+//funkcja, dzieki ktorej bedziemy widziec aktualny czesc naszej przygody wyswietlanej w formie sciany tekstu
+function showTextNode(textNodeIndex) {
+
+}
+
+//funckja, dzieki ktorej bedziemy widziec jakie opcje (w buttonach) mamy do wyboru
+function showOption(option) {
+
+}
+
+function selectOption(option) {
+
+}
+
+//ELEMENTY NASZEJ PRZYGODY
+const textNodes = [
+    {
+
+    }
+]
+
+startGame()
