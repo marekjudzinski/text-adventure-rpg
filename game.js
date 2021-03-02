99707aa59052f3e129a08559eeb2237351ed54a6
Marek <marekju80@gmail.com>
2021-03-02 15:02:04 +0100

    pierwsze mozliwe sciezki

--- game.js
+++ game.js
@@ -33,7 +33,11 @@ function showOption(option) {
 }
 
 function selectOption(option) {
-
+const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0){
+        return startGame()
+    }
+    showTextNode(nextTextNodeId)
 }
 
 //ELEMENTY NASZEJ PRZYGODY
@@ -51,6 +55,46 @@ const textNodes = [
                 nextText: 3
             }
         ]
+    },
+    {
+        id: 2,
+        text: 'Po drodze masz czas na refleksje. Twoje zycie jest nudne i smutne, w domu nigdy sie nie przelewalo, ojciec bil matke, gdy tylko konczyla sie wodka, ale czare goryczy przelewa fakt, ze caly czas boli cie glowa... Ale czemu boli cie dupa...?',
+        options: [
+            {
+                text: 'kontynuuj glebokie przemyslenia...',
+                nextText: 4
+            }
+        ]
+    },
+    {
+        id: 3,
+        text: 'Umarles i obys nigdy nie powstal z martwych... kto jeszcze dabuje w 2021?',
+        options: [
+            {
+                text: 'RESTART THE GAME',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id:4,
+        text: 'Mieszkasz na wsi. Jest to o tyle dobre, ze masz blisko do kosciola... ale do burdelu musisz isc az do miasta. Zauwazasz, ze skonczyla ci sie gotowka... czy ostatniej nocy miales za co zaplacic?',
+        options: [
+            {
+                //tu ma sie zaczac zabawa
+                text: 'idz dalej sciezka',
+                nextText: 5
+            },
+            {
+                //a tu pieklo...
+                text: 'idz skrotem przez las',
+                nextText: 6
+            }
+        ]
+    },
+    {
+        id: 5,
+        text: ''
     }
 ]
 
